4e35110c71ef894ec2bc24dd555b22cc99af39e3
USER <USER@ZULBIASA02>
2022-04-05 12:02:12 +0800

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n As Integer
+
+        For n = 50 To 0 Step -5
+            TextBox1.Text = TextBox1.Text & n & vbCr & vbCrLf
+        Next n
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
